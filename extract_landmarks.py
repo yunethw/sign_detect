c2aeb0c9dcd34f07dd59e42071f37e7f0524800f
Yuneth Wijenayake <yunethcwij@gmail.com>
2023-04-04 01:23:37 +0530

    automated recording

---
+++ extract_landmarks.py
@@ -0,0 +1,216 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import pandas as pd
+
+landmarks_relative = []
+landmarks_world_left = []
+landmarks_world_right = []
+
+
+# def extract_landmarks(frames, path):
+#     dynm_cols = ['LETTER']
+#
+#     for idx_d_col_1 in range(21):
+#         dynm_cols = dynm_cols + [f'L{idx_d_col_1}X'] + [f'L{idx_d_col_1}Y']
+#
+#     for idx_d_col_2 in range(21):
+#         dynm_cols = dynm_cols + [f'R{idx_d_col_2}X'] + [f'R{idx_d_col_2}Y']
+#
+#     for idx_d_col_3 in range(21):
+#         dynm_cols = dynm_cols + [f'{idx_d_col_3}X'] + [f'{idx_d_col_3}Y'] + [f'L{idx_d_col_3}Z'] + [f'R{idx_d_col_3}Z']
+#
+#     df = pd.DataFrame(columns=dynm_cols)
+#
+#     hands = mp.solutions.hands.Hands()
+#
+#     for i, frame in enumerate(frames):
+#         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         results = hands.process(frame)
+#
+#         if results.multi_hand_world_landmarks:
+#             if len(results.multi_hand_world_landmarks) == 2:
+#                 landmarks_ar_wlx = [lmk.x for lmk in results.multi_hand_world_landmarks[0].landmark]
+#                 landmarks_ar_wly = [lmk.y for lmk in results.multi_hand_world_landmarks[0].landmark]
+#                 landmarks_ar_wrx = [lmk.x for lmk in results.multi_hand_world_landmarks[1].landmark]
+#                 landmarks_ar_wry = [lmk.y for lmk in results.multi_hand_world_landmarks[1].landmark]
+#
+#                 landmarks_ar_lx = [lmk.x for lmk in results.multi_hand_landmarks[0].landmark]
+#                 landmarks_ar_rx = [lmk.x for lmk in results.multi_hand_landmarks[1].landmark]
+#                 landmarks_ar_ly = [lmk.y for lmk in results.multi_hand_landmarks[0].landmark]
+#                 landmarks_ar_ry = [lmk.y for lmk in results.multi_hand_landmarks[1].landmark]
+#
+#                 landmarks_ar_lz = [lmk.z for lmk in results.multi_hand_landmarks[0].landmark]
+#                 landmarks_ar_rz = [lmk.z for lmk in results.multi_hand_landmarks[1].landmark]
+#             elif len(results.multi_hand_world_landmarks) == 1:
+#                 landmarks_ar_wlx = [0] * 21
+#                 landmarks_ar_wly = [0] * 21
+#                 landmarks_ar_lx = [0] * 21
+#                 landmarks_ar_ly = [0] * 21
+#                 landmarks_ar_lz = [0] * 21
+#
+#                 landmarks_ar_wrx = [lmk.x for lmk in results.multi_hand_world_landmarks[0].landmark]
+#                 landmarks_ar_wry = [lmk.y for lmk in results.multi_hand_world_landmarks[0].landmark]
+#                 landmarks_ar_rx = [lmk.x for lmk in results.multi_hand_landmarks[0].landmark]
+#                 landmarks_ar_ry = [lmk.y for lmk in results.multi_hand_landmarks[0].landmark]
+#
+#                 landmarks_ar_rz = [lmk.z for lmk in results.multi_hand_landmarks[0].landmark]
+#             else:
+#                 landmarks_ar_wlx = [0] * 21
+#                 landmarks_ar_wly = landmarks_ar_wlx
+#                 landmarks_ar_lx = landmarks_ar_wlx
+#                 landmarks_ar_ly = landmarks_ar_wlx
+#                 landmarks_ar_lz = landmarks_ar_wlx
+#
+#                 landmarks_ar_wrx = landmarks_ar_wlx
+#                 landmarks_ar_wry = landmarks_ar_wlx
+#                 landmarks_ar_rx = landmarks_ar_wlx
+#                 landmarks_ar_ry = landmarks_ar_wlx
+#                 landmarks_ar_rz = landmarks_ar_wlx
+#
+#         for idx_l in range(21):
+#             landmarks_world_left.append(landmarks_ar_wlx[idx_l])
+#             landmarks_world_left.append(landmarks_ar_wly[idx_l])
+#
+#         for idx_r in range(21):
+#             landmarks_world_right.append(landmarks_ar_wrx[idx_r])
+#             landmarks_world_right.append(landmarks_ar_wry[idx_r])
+#
+#         for idx in range(21):
+#             landmarks_relative.append(abs(landmarks_ar_lx[idx] - landmarks_ar_rx[idx]))
+#             landmarks_relative.append(abs(landmarks_ar_ly[idx] - landmarks_ar_ry[idx]))
+#             landmarks_relative.append(landmarks_ar_lz[idx])
+#             landmarks_relative.append(landmarks_ar_rz[idx])
+#
+#         landmarks_array = [ord(path[0])-96] + landmarks_world_left + landmarks_world_right + landmarks_relative
+#
+#         landmarks_world_left.clear()
+#         landmarks_world_right.clear()
+#         landmarks_relative.clear()
+#
+#         df.loc[i] = landmarks_array
+#
+#     excel_path = 'hand_marks.xlsx'
+#     with pd.ExcelWriter(excel_path, mode='a') as writer:
+#         df.to_excel(writer, sheet_name='Landmarks', index=False)
+def extract_landmarks(frames, height, width, datafile, letter):
+
+    def select_frames(all_frames, frame_height, frame_width):
+        rand_int = np.random.choice(len(all_frames), 50, replace=False)
+        frames_array = np.empty((50, frame_height, frame_width, 3), dtype=np.uint8)
+        j = 0
+        for i in range(len(all_frames)):
+            if i in rand_int:
+                frames_array[j] = all_frames[i]
+                j += 1
+        return frames_array
+
+    selected_frames = select_frames(frames, height, width)
+
+    dynm_cols = ['LETTER']
+    for idx_d_col_1 in range(21):
+        dynm_cols = dynm_cols + [f'L{idx_d_col_1}X'] + [f'L{idx_d_col_1}Y']
+    for idx_d_col_2 in range(21):
+        dynm_cols = dynm_cols + [f'R{idx_d_col_2}X'] + [f'R{idx_d_col_2}Y']
+    for idx_d_col_3 in range(21):
+        dynm_cols = dynm_cols + [f'{idx_d_col_3}X'] + [f'{idx_d_col_3}Y'] + [f'L{idx_d_col_3}Z'] + [f'R{idx_d_col_3}Z']
+
+    df = pd.DataFrame(columns=dynm_cols)
+
+    hands = mp.solutions.hands.Hands()
+    for i, frame in enumerate(selected_frames):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame)
+
+        if results.multi_hand_world_landmarks:
+            if len(results.multi_hand_world_landmarks) == 2:
+                landmarks_ar_wlx = [lmk.x for lmk in results.multi_hand_world_landmarks[0].landmark]
+                landmarks_ar_wly = [lmk.y for lmk in results.multi_hand_world_landmarks[0].landmark]
+                landmarks_ar_wrx = [lmk.x for lmk in results.multi_hand_world_landmarks[1].landmark]
+                landmarks_ar_wry = [lmk.y for lmk in results.multi_hand_world_landmarks[1].landmark]
+
+                landmarks_ar_lx = [lmk.x for lmk in results.multi_hand_landmarks[0].landmark]
+                landmarks_ar_rx = [lmk.x for lmk in results.multi_hand_landmarks[1].landmark]
+                landmarks_ar_ly = [lmk.y for lmk in results.multi_hand_landmarks[0].landmark]
+                landmarks_ar_ry = [lmk.y for lmk in results.multi_hand_landmarks[1].landmark]
+
+                landmarks_ar_lz = [lmk.z for lmk in results.multi_hand_landmarks[0].landmark]
+                landmarks_ar_rz = [lmk.z for lmk in results.multi_hand_landmarks[1].landmark]
+            elif len(results.multi_hand_world_landmarks) == 1:
+                landmarks_ar_wlx = [0] * 21
+                landmarks_ar_wly = [0] * 21
+                landmarks_ar_lx = [0] * 21
+                landmarks_ar_ly = [0] * 21
+                landmarks_ar_lz = [0] * 21
+
+                landmarks_ar_wrx = [lmk.x for lmk in results.multi_hand_world_landmarks[0].landmark]
+                landmarks_ar_wry = [lmk.y for lmk in results.multi_hand_world_landmarks[0].landmark]
+                landmarks_ar_rx = [lmk.x for lmk in results.multi_hand_landmarks[0].landmark]
+                landmarks_ar_ry = [lmk.y for lmk in results.multi_hand_landmarks[0].landmark]
+
+                landmarks_ar_rz = [lmk.z for lmk in results.multi_hand_landmarks[0].landmark]
+            else:
+                landmarks_ar_wlx = [0] * 21
+                landmarks_ar_wly = landmarks_ar_wlx
+                landmarks_ar_lx = landmarks_ar_wlx
+                landmarks_ar_ly = landmarks_ar_wlx
+                landmarks_ar_lz = landmarks_ar_wlx
+
+                landmarks_ar_wrx = landmarks_ar_wlx
+                landmarks_ar_wry = landmarks_ar_wlx
+                landmarks_ar_rx = landmarks_ar_wlx
+                landmarks_ar_ry = landmarks_ar_wlx
+                landmarks_ar_rz = landmarks_ar_wlx
+
+        for idx_l in range(21):
+            landmarks_world_left.append(landmarks_ar_wlx[idx_l])
+            landmarks_world_left.append(landmarks_ar_wly[idx_l])
+
+        for idx_r in range(21):
+            landmarks_world_right.append(landmarks_ar_wrx[idx_r])
+            landmarks_world_right.append(landmarks_ar_wry[idx_r])
+
+        for idx in range(21):
+            landmarks_relative.append(abs(landmarks_ar_lx[idx] - landmarks_ar_rx[idx]))
+            landmarks_relative.append(abs(landmarks_ar_ly[idx] - landmarks_ar_ry[idx]))
+            landmarks_relative.append(landmarks_ar_lz[idx])
+            landmarks_relative.append(landmarks_ar_rz[idx])
+
+        landmarks_array = [ord(letter)-64] + landmarks_world_left + landmarks_world_right + landmarks_relative
+
+        landmarks_world_left.clear()
+        landmarks_world_right.clear()
+        landmarks_relative.clear()
+
+        df.loc[i] = landmarks_array
+
+    excel_path = datafile
+    with pd.ExcelWriter(excel_path, mode='a') as writer:
+        df.to_excel(writer, sheet_name='Landmarks', index=False)
+
+
+def frame_capture(path):
+    cap = cv2.VideoCapture(path)
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    frames_array = np.empty((50, frame_height, frame_width, 3), dtype=np.uint8)
+    rand_int = np.random.choice(num_frames, 50, replace=False)
+    j = 0
+    for i in range(num_frames):
+        ret, frame = cap.read()
+        if ret and i in rand_int:
+            frames_array[j] = frame
+            j += 1
+
+    return frames_array
+
+
+def initiate_hand_marks(path):
+    selected_frames = frame_capture(path)
+    extract_landmarks(selected_frames, path)
+
+
+if __name__ == '__main__':
+    initiate_hand_marks('a.mp4')
--- record.py
+++ record.py
@@ -1,29 +1,60 @@
 import cv2 as cv
+from mediapipe import solutions
+import numpy as np
+import extract_landmarks as extract
 
-start_tick = 0
-letter = '0'
+mp_hands = solutions.hands
+mp_drawing = solutions.drawing_utils
+
+def mediapipe_detect(image, mp_model):
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = mp_model.process(image)
+    image.flags.writeable = True
+    image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+    return image, results
+
+
+def draw_landmarks(image, results):
+    for hand_landmarks in results:
+        mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
 
 def start():
-    global start_tick, letter
+    start_tick = 0
+    printed = False
+    letter = '0'
     cap = cv.VideoCapture(0)
     freq = cv.getTickFrequency()
     text_top_position = (5, 30)
     text_below_position = (5, 60)
     font = cv.FONT_HERSHEY_SIMPLEX
     color = (255, 255, 255)
+    datafile_path = 'data.xlsx'
 
+    frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    frames_array = np.empty((200, frame_height, frame_width, 3), dtype=np.uint8)  # declaring frame storage with extra memory
+    frame_num = 0
+
+    hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
     while cap.isOpened():
         ret, frame = cap.read()
         frame = cv.flip(frame, 1)
-
+        frame, results = mediapipe_detect(frame, hands)
+        frame_copy = frame
+        if results.multi_hand_landmarks:
+            draw_landmarks(frame, results.multi_hand_landmarks)
         key = cv.waitKey(10)
         if key > 0:
             if key == ord('Q'):
                 break
             elif ord('a') <= key <= ord('z'):
                 letter = chr(key).upper()
+                printed = False
                 start_tick = cv.getTickCount()
+
+
 
         time = int((cv.getTickCount() - start_tick) / freq)
         if time < 15:  # start recording procedure
@@ -32,8 +63,17 @@ def start():
                 frame = cv.putText(frame, 'Recording in {}'.format(3 - time), text_below_position, font, 1, color)
             elif 3 < time <= 8:  # recording 5 seconds
                 frame = cv.putText(frame, '{}'.format(time - 3), text_below_position, font, 1, color)
+                frames_array[frame_num] = frame_copy
+                frame_num += 1
             else:  # recording complete
-                frame = cv.putText(frame, 'Recording Complete', text_below_position, font, 1, color)
+                if not printed:
+                    print('Letter {}: {} frames recorded'.format(letter, frame_num))
+                    printed = True
+                    frame = cv.putText(frame, 'Processing...', text_below_position, font, 1, color)
+                    print('Extracting landmarks...')
+                    extract.extract_landmarks(frames_array[0:frame_num], frame_height, frame_width, datafile_path, letter)
+                    print('Done.')
+
         else:
             frame = cv.putText(frame, 'Press letter key to record', text_top_position, font, 1, color)
 
