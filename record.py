4e437c5c23de60eb3221ee42f7ca6a6c85929969
Yuneth Wijenayake <yunethcwij@gmail.com>
2023-04-05 15:59:44 +0530

    Fixed array not clearing error

--- record.py
+++ record.py
@@ -2,8 +2,15 @@ import cv2 as cv
 import mediapipe as mp
 import data
 
+TEXT_TOP_POS = (5, 30)
+TEXT_BELOW_POS = (5, 60)
+FONT = cv.FONT_HERSHEY_SIMPLEX
+COLOR = (255, 255, 255)
+DATAFILE_PATH = 'data.xlsx'
+
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
+
 
 def mediapipe_detect(image, mp_model):
     image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
@@ -21,19 +28,16 @@ def draw_landmarks(image, results):
 
 def start():
     start_tick = 0
+    freq = cv.getTickFrequency()
+
     printed = False
     extracted = False
-    letter = '0'
-    cap = cv.VideoCapture(0)
-    freq = cv.getTickFrequency()
-    text_top_position = (5, 30)
-    text_below_position = (5, 60)
-    font = cv.FONT_HERSHEY_SIMPLEX
-    color = (255, 255, 255)
-    datafile_path = 'data.xlsx'
 
+    letter = '0'
     results_array = []
     dataframe = data.SignData()
+
+    cap = cv.VideoCapture(0)
 
     hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
     while cap.isOpened():
@@ -51,32 +55,33 @@ def start():
                 letter = chr(key).upper()
                 printed = False
                 extracted = False
+                results_array.clear()
                 start_tick = cv.getTickCount()
 
         time = int((cv.getTickCount() - start_tick) / freq)
         if time < 13:  # start recording procedure
-            frame = cv.putText(frame, letter, text_top_position, font, 1, color)
+            frame = cv.putText(frame, letter, TEXT_TOP_POS, FONT, 1, COLOR)
             if time <= 3:  # count down
-                frame = cv.putText(frame, 'Recording in {}'.format(3 - time), text_below_position, font, 1, color)
+                frame = cv.putText(frame, 'Recording in {}'.format(3 - time), TEXT_BELOW_POS, FONT, 1, COLOR)
             elif 3 < time <= 8:  # recording 5 seconds
-                frame = cv.putText(frame, '{}'.format(time - 3), text_below_position, font, 1, color)
+                frame = cv.putText(frame, '{}'.format(time - 3), TEXT_BELOW_POS, FONT, 1, COLOR)
                 results_array.append(results)
             else:  # recording complete
                 if not printed:
                     print('Letter {}: {} frames recorded'.format(letter, len(results_array)))
                     printed = True
-                    frame = cv.putText(frame, 'Processing...', text_below_position, font, 1, color)
+                    frame = cv.putText(frame, 'Processing...', TEXT_BELOW_POS, FONT, 1, COLOR)
                 elif printed and not extracted:
                     print('Extracting landmarks...')
                     dataframe.add_data(results_array, letter)
                     extracted = True
                     print('Done.')
         else:
-            frame = cv.putText(frame, 'Press letter key to record', text_top_position, font, 1, color)
+            frame = cv.putText(frame, 'Press letter key to record', TEXT_TOP_POS, FONT, 1, COLOR)
 
         cv.imshow('Feed', frame)
 
-    dataframe.save_data(datafile_path)
+    dataframe.save_data(DATAFILE_PATH)
     cap.release()
     cv.destroyAllWindows()
 
